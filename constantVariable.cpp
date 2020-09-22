9cde2bc07a33fd92bb4165b337d608758c9b528a
shri7870 <71579575+shri7870@users.noreply.github.com>
2020-09-22 12:36:43 +0530

    Create constantVariable.cpp

---
+++ constantVariable.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int age =23;
+  float average; 
+  char sex = 'm';
+  bool isit =true;
+  average= 22.30;
+  cout <<age << endl << average <<endl << sex << endl << isit;
+
+
+    return 0;
+}
+ 
