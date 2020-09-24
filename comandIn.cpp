86d64ee59b98f0f603d7458a5a56ee4a36356128
shri7870 <71579575+shri7870@users.noreply.github.com>
2020-09-24 11:34:00 +0530

    Create comandIn.cpp

---
+++ comandIn.cpp
@@ -0,0 +1,17 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+  int age =23;// this is a comand.
+  float average; 
+  char sex = 'm';
+  bool isit =true;
+  average= 22.30;
+  cout <<age << endl << average <<endl << sex << endl << isit;
+
+    return 0;
+}
+ 
