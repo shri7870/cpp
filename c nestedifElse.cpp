10bd34051ea0499a18819f511b7ee94d2a876ff1
shri7870 <71579575+shri7870@users.noreply.github.com>
2020-10-19 22:38:59 +0530

    Create c nestedifElse.cpp

---
+++ c nestedifElse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age = 45;
+    if (age<=20){
+        cout<<"you are young";
+
+    }
+    else if (age<=40){
+    cout<<"you are the middle age";
+    }
+    else{
+        cout<<"you are old gys";
+    }
+    return 0;
+}
