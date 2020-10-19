720f4f2873e981718eedd85f0c27c83bd639facb
shri7870 <71579575+shri7870@users.noreply.github.com>
2020-10-19 22:17:34 +0530

    Create ifLesestatement.cpp

---
+++ ifLesestatement.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+    int age =14;
+    if (age<=17){
+        cout<<"you can vote go to home"<<endl;
+
+    }
+    else{
+        cout<<"yes you can give the vote"<<endl;
+        return 0;
+    }
+}
+
+
